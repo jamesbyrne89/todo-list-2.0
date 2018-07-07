87950f2a3d00eb51d0a86a1fb8c9ea668526337b
jamesbyrne89 <mail@jamestbyrne.com>
2018-07-07 22:42:40 +0200

    Add remaining variables for setup

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express'),
-mongoose = require('mongoose'),
-bodyParser = require('body-parser'),
-dotenv = require ('dotenv')
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  dotenv = require('dotenv');
 
-
-const env = dotenv.config({path: 'variables.env'});
+const env = dotenv.config({ path: 'config/variables.env' });
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const db = env.DATABASE;
+console.log(env.DATABASE)
+
+// Connect mongoose to database
+// mongoose.connect(db).then(()=> console.log('db connected'));
