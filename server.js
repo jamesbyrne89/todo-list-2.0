99cfd49644ba644293a4e6b0fe2b0aeffa573e57
jamesbyrne89 <mail@jamestbyrne.com>
2018-07-07 21:22:40 +0100

    Setup db in mlab and add nodemon

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express'),
 mongoose = require('mongoose'),
-bodyParser = require('body-parser')
+bodyParser = require('body-parser'),
+dotenv = require ('dotenv')
 
+
+const env = dotenv.config({path: 'variables.env'});
 
 const app = express();
 
 app.use(bodyParser.json())
+
+const db = env.DATABASE;
