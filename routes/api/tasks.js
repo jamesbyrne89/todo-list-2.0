5e549465eca3ec82d07c12fbbeeb2ac649351b58
James Byrne <mail@jamestbyrne.com>
2018-07-08 18:41:06 +0200

    Complete api

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -10,7 +10,32 @@ const Task = require('../../models/task');
 // @access Public
 
 router.get('/', (req, res) => {
-  Task.find().sort({date: -1}).then(tasks => res.json(items));
+  Task.find()
+    .sort({ date: -1 })
+    .then(tasks => res.json(items));
+});
+
+// @route POST api/tasks
+// desc Create new task
+// @access Public
+
+router.post('/', (req, res) => {
+  const newTask = new Task({
+    name: req.body.name,
+    completed: false
+  });
+  newTask.save().then(task => res.json(task));
+});
+
+// @route DELETE api/tasks/:id
+// desc Delete a task
+// @access Public
+
+router.delete('/:id', (req, res) => {
+  Task.findById(req.params.id)
+    .then(task => task.remove())
+    .then(() => console.log(res.json({ success: true })))
+    .catch(err => res.status(404).json({success: false}) );
 });
 
 module.exports = router;
