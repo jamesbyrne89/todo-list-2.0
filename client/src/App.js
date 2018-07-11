46fddb9d68fb5f45b2d78ccf7c0d0ef5b5a1c583
jamesbyrne89 <mail@jamestbyrne.com>
2018-07-11 14:17:50 +0100

    Add modal

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import './styles.css';
 import NavBar from './components/NavBar';
 import List from './components/List';
-
+import NewTaskModal from './components/Modal';
+import AddTask from './components/AddTask';
 
 class App extends Component {
- 
   render() {
     return (
       <div className="app-container">
         <NavBar />
         <main className="list-container">
           <List />
-
         </main>
+        <NewTaskModal trigger={AddTask} />
       </div>
     );
   }
---
+++ client/src/components/Modal.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Modal } from 'react-materialize';
+
+const NewTaskModal = () => <Modal />;
+
+export default NewTaskModal;
