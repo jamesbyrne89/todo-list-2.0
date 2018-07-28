5141b249209871a65123a927b8a05f1ef0bc8fd7
jamesbyrne89 <mail@jamestbyrne.com>
2018-07-28 07:48:23 +0100

    Set up production environment config

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
   dotenv = require('dotenv'),
-  tasks = require('./routes/api/tasks');
+  tasks = require('./routes/api/tasks'),
+  path = require('path');
 
 const env = dotenv.config({ path: 'config/variables.env' });
 const db = env.parsed.DATABASE;
 const app = express();
+
 
 // BodyParser middleware
 app.use(bodyParser.json());
@@ -22,6 +24,16 @@ mongoose
 
 // Use routes
 app.use('/api/tasks', tasks);
+
+//Serve static assets if in production environment
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // Define port
 const port = process.env.port || 5000;
