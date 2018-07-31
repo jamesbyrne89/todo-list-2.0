6e7f94fc07e5198974561ecdf8751cfb6d17b9c7
jamesbyrne89 <mail@jamestbyrne.com>
2018-07-31 22:15:25 +0100

    Heroku build

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const env = dotenv.config({ path: 'config/variables.env' });
 const db = env.parsed.DATABASE;
 const app = express();
 
-
 // BodyParser middleware
 app.use(bodyParser.json());
 
@@ -31,11 +30,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // Define port
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
