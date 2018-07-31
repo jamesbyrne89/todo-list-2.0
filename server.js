42db05ad02ed2d0f3e5a5f4fdae9103110fa47e3
jamesbyrne89 <mail@jamestbyrne.com>
2018-07-31 22:31:47 +0100

    Log dirname

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
