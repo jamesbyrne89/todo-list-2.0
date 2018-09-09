50d6d9ae16df66c60813d4dc8f2fefac38ff6f2e
jamesbyrne89 <mail@jamestbyrne.com>
2018-09-09 11:38:52 +0100

    Add validator package for usre signup

--- models/User.js
+++ models/User.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const validator = require('validator');
 
 const UserSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
+  firstName: {
+    type: String,
+    trim: true,
+    required: 'Please enter your first name'
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: 'Please enter your last name'
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    validate: [
+      validator.isEmail,
+      `That doesn't appear to be a valid email address`
+    ],
+    required: 'Please provide an email address'
+  },
   joined: { type: Object, required: true },
   tasks: { type: Array, required: true }
 });
