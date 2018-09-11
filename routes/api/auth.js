23883ed119bde62bc7f52f5f3267bc4b6441f6eb
James Byrne <mail@jamestbyrne.com>
2018-09-11 20:20:38 +0100

    Refactor passport functions

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.post(
-  '/login',
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  (req, res) => {
-    res.redirect('/');
-  }
-);
+router.post('/login', passport.authenticate('local-signup', {
+  successRedirect : '/', // redirect to the secure profile section
+  failureRedirect : '/login', // redirect back to the signup page if there is an error
+  failureFlash : true // allow flash messages
+}));
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const env = dotenv.config({ path: 'config/variables.env' });
 const db = env.parsed.DATABASE;
 const app = express();
 const passport = require('passport');
-const Strategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 // BodyParser middleware
 app.use(bodyParser.json());
@@ -48,23 +48,60 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Configure the local strategy for Passport
 passport.use(
-  new Strategy((username, password, cb) => {
-    db.users.findByUsername(username, (err, user) => {
-      err ? cb(err) : cb(null, false);
-      if (user.password !== password) {
-        return cb(null, false);
-      }
-      return cb(null, user);
-    });
-  })
+  'local-signup',
+  new LocalStrategy(
+    {
+      // by default, local strategy uses username and password, we will override with email
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true // allows us to pass back the entire request to the callback
+    },
+    (req, email, password, done) => {
+      // asynchronous
+      // User.findOne wont fire unless data is sent back
+      process.nextTick(() => {
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'email': email }, (err, user) => {
+          // if there are any errors, return the error
+          if (err) return done(err);
+
+          // check to see if theres already a user with that email
+          if (user) {
+            return done(
+              null,
+              false,
+              req.flash('signupMessage', 'That email is already taken.')
+            );
+          } else {
+            // if there is no user with that email
+            // create the user
+            var newUser = new User();
+
+            // set the user's local credentials
+            newUser.local.email = email;
+            newUser.local.password = newUser.generateHash(password);
+
+            // save the user
+            newUser.save(err => {
+              if (err) throw err;
+              return done(null, newUser);
+            });
+          }
+        });
+      });
+    }
+  )
 );
+
 // Configure Passport authenticated session persistence
 
-passport.serializeUser((id, cb) => {
-  db.users.findById(id, (err, user) => {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, user);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+      done(err, user);
   });
 });
