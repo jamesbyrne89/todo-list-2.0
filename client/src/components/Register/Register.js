17e3782001f74403278b5c1370103bcf424daa4f
jamesbyrne89 <mail@jamestbyrne.com>
2018-09-28 20:04:10 +0100

    Add handlesubmit function to Register route

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Input, Card } from 'react-materialize';
-import NavBar from '../NavBar/NavBar';
+import axios from 'axios';
 
 class Register extends Component {
   constructor() {
@@ -20,40 +20,45 @@ class Register extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  handleSubmit = () => {
+    const { email, password } = this.state.credentials;
+    axios
+      .post('/auth/login', { email, password })
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+  };
+
   render() {
     return (
-      <div className="app-container">
-        <NavBar title="Todo List" />
-        <Card className="login-container">
-          <Input
-            s={6}
-            name="firstName"
-            label="First Name"
-            className="signin-input"
-            autoFocus
-          />
-          <Input
-            s={6}
-            name="lastName"
-            label="Last name"
-            className="signin-input"
-          />
-          <Input s={6} name="email" label="Email" className="signin-input" />
-          <Input
-            s={6}
-            name="password"
-            label="Password"
-            className="signin-input"
-          />
-          <Input
-            s={6}
-            name="confirmPassword"
-            label="Repeat password"
-            className="signin-input"
-          />
-          <Button className="login-btn">Log in</Button>
-        </Card>
-      </div>
+      <Card className="login-container">
+        <Input
+          s={6}
+          name="firstName"
+          label="First Name"
+          className="signin-input"
+          autoFocus
+        />
+        <Input
+          s={6}
+          name="lastName"
+          label="Last name"
+          className="signin-input"
+        />
+        <Input s={6} name="email" label="Email" className="signin-input" />
+        <Input
+          s={6}
+          name="password"
+          label="Password"
+          className="signin-input"
+        />
+        <Input
+          s={6}
+          name="confirmPassword"
+          label="Repeat password"
+          className="signin-input"
+        />
+        <Button className="login-btn">Log in</Button>
+      </Card>
     );
   }
 }
