72ad03f2ceadb6325d2371c301489c4084748c2c
James Byrne <mail@jamestbyrne.com>
2018-10-02 10:08:11 +0100

    Add tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+import NavBar from './components/NavBar/NavBar';
+import { Provider } from 'react-redux';
+import store from './store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('snapshot', () => {
+  const nav = renderer.create(<NavBar />);
+  expect(nav.toJSON()).toMatchSnapshot();
 });
